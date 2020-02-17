5822dd36554d69426beb74f563a2acb21e8e853a
wangbin <wangbin5584039@163.com>
2020-02-17 13:05:42 +0800

    键盘录入

---
+++ src/Demo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Demo{
+
+    public static void main(String[]args){
+
+        Demo demo = new Demo();
+
+        demo.print("wangbin", 30);
+
+        System.out.println("请输入你的名字：");
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.next();
+
+        System.out.println("请输入你的年龄：");
+        int age = scanner.nextInt();
+
+        demo.print(name, age);
+
+    }
+
+
+    public void print( String name, int age){
+
+        System.out.println("你的名字是：" + name + ", 年龄是：" + age);
+
+    }
+
+}
