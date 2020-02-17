990d5a324c01e55434b3d5287289e32d51832e46
wangbin <wangbin5584039@163.com>
2020-02-17 12:43:06 +0800

    Initial commit

---
+++ src/HelloWord.java
@@ -0,0 +1,11 @@
+public class HelloWord {
+
+    public void helloWord(){
+        System.out.println("hello word");
+    }
+
+    public static void main(String[] args) {
+        HelloWord helloWord = new HelloWord();
+        helloWord.helloWord();
+    }
+}
